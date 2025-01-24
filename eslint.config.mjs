14fc5542297fec5c5aa978a4c62133d10d2443e6
Keal Jones <kealjones@gmail.com>
2025-01-24 09:07:38 -0700

    switch to using npm instead of pnpm to accommodate vsce and fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,8 +2,8 @@ import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
 
 export default [{
-    files: ["**/*.ts"],
-}, {
+    files: ["src/**/*.{ts|js}", "media/**/*.{ts|js}"],
+
     plugins: {
         "@typescript-eslint": typescriptEslint,
     },
